e6ba67b11151ab4888197f401bf17aeb4a516ac7
Rafael30s <rafael.santos@bandtec.com.br>
2020-06-18 20:19:12 -0300

    Implementando cadastro

---
+++ src/database/db.js
@@ -0,0 +1,83 @@
+// importar a dependencia do sqlite3
+const sqlite3 = require("sqlite3").verbose()
+
+// Criar o objeto que irá fazer operações no banco de dados
+const db = new sqlite3.Database("./src/database/database.db")
+
+module.exports = db;
+// utilizar o objeto de banco de dados, para nossas operações
+db.serialize(() => {
+    // com comandos SQL vou:
+
+    //1. criar uma tabela 
+    //
+    // db.run(`
+
+    //     CREATE TABLE IF NOT EXISTS User(
+    //         idUser INTEGER PRIMARY KEY AUTOINCREMENT,
+    //         nome varchar(100),
+    //         username varchar(30),
+    //         email varchar(150),
+    //         senha varchar(30));
+            
+
+    // `)
+    // CREATE TABLE IF NOT EXISTS  Story(
+    //     idStory INTEGER PRIMARY KEY AUTOINCREMENT,
+    //     titulo varchar(100),
+    //     descrição tinytext,
+    //     historia text,
+    //     image varchar(150),
+    //     fkUser int,
+    //     foreign key(fkUser) references User(idUser)
+    //     );
+//     //2. Inserir dados na tabela
+    // const query = `
+    //     INSERT INTO User (
+    //         nome, 
+    //         username,
+    //         email,
+    //         senha
+    //     ) VALUES (?,?,?,?);
+    // `;
+
+    // const values = [
+    //     "Rafael dos Santos",
+    //     "Rafael30s",
+    //     "rafael30s@hotmail.com.br",
+    //     "12345"
+    // ]
+
+    // function afterInsertData(err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+
+    //     console.log("Cadastrado com sucesso");
+    //     console.log(this);
+    // }
+    // db.run(query, values, afterInsertData);
+
+   
+
+//     // //3. consultar os dados da tabela
+    db.all(`SELECT * FROM user`, function (err, rows) {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Aqui estão seus registros");
+        console.log(rows);
+    })
+
+//     // //4. Deletar um dado da tabela
+    // db.run(`DELETE FROM places WHERE id = ?`, [6], function (err) {
+    //     if (err) {
+    //         return console.log(err)
+    //     }
+
+    //     console.log("Registro deletado com sucesso");
+    // })
+
+
+ })
--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const server = express();
 
+// pegar o banco de dados
+const db = require("./database/db");
+
 // configurar pasta publica
 server.use(express.static("public"));
+
+// criando cookie
+
+// let idUser = document.cookie = '';
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }));
 
 // utilizando template engine
 const nunjucks = require("nunjucks");
@@ -15,10 +25,55 @@ server.get("/", (req, res) => {
 });
 
 server.get("/cadastrar", (req, res) => {
+
+    console.log(req.query);
     return res.render("cadastro.html");
 });
 
+server.post("/cadastrar", (req, res) => {
+
+    console.log(req.body);
+    let validar_senha = (req.body.senha = req.body.confirmaSenha);
+    
+    if (validar_senha) {
+        const query = `
+        INSERT INTO User (
+            nome, 
+            username,
+            email,
+            senha
+        ) VALUES (?,?,?,?);
+    `;
+
+        const values = [
+            req.body.nome,
+            req.body.username,
+            req.body.email,
+            req.body.senha
+        ]
+
+        function afterInsertData(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("Cadastrado com sucesso");
+            console.log(this);
+        }
+        db.run(query, values, afterInsertData);
+
+        return res.render("cadastro.html", {saved:true});
+    }
+    else {
+        alert('Falha na confirmação de senha');
+    }
+});
+
 server.get("/login", (req, res) => {
+    // let idUser = document.cookie = 'teste';
+
+    // console.log(idUser);
+
     return res.render("login.html");
 });
 
@@ -32,6 +87,10 @@ server.get("/exemploTexto", (req, res) => {
 
 server.get("/cadastrarHistoria", (req, res) => {
     return res.render("cadastroHistoria.html");
+});
+
+server.get("/historia", (req, res) => {
+    return res.render("layoutHistoria.html");
 });
 
 server.get("/exemploVideo", (req, res) => {
